2a3a7632421061fa0d38ca6950f5b016b988586f
sanlouise <sandra.adamshallie@gmail.com>
2017-07-18 14:39:32 -0400

    Finish main functionality

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const ul = document.getElementById('recipes');
 const submit = document.getElementById('submit');
+const wrapper = document.querySelector(".wrapper");
 submit.addEventListener('click', fetchData);
-
-
 
 function fetchData(event){
   let searchterm = document.getElementById('input').value;
@@ -11,11 +10,31 @@ function fetchData(event){
   fetch(url)
   .then((response) => response.json())
   .then(data => {
+    for (let i = 0; i < data.results.length; i++) {
 
+      let wholeRecipe = document.createElement("div")
+      let wholeRecipeLink = document.createElement("a")
+      let recipeTitle = document.createElement("li");
+      let recipeThumbnail = document.createElement("li");
+
+      wrapper.appendChild(wholeRecipe);
+      wholeRecipe.appendChild(wholeRecipeLink);
+      wholeRecipeLink.appendChild(recipeTitle);
+      wholeRecipeLink.appendChild(recipeThumbnail);
+
+      wholeRecipeLink.setAttribute("href", data.results[i].href);
+      recipeTitle.innerHTML += data.results[i].title
+
+      if (!data.results[i].thumbnail) {
+        recipeThumbnail.innerHTML += `<img src="${'http://via.placeholder.com/200x200'}" />`
+      }
+      else {
+        recipeThumbnail.innerHTML += `<img src="${data.results[i].thumbnail}" />`
+      }
+    }
   })
 
   .catch(error => {
     console.log(error);
-
   });
 }
