b4b8a3e38c0a851478c081610e00480b1ce6fb6e
sanlouise <sandra.adamshallie@gmail.com>
2017-07-18 13:36:21 -0400

    Initial commit

--- main.js
+++ main.js
@@ -2,17 +2,20 @@ const ul = document.getElementById('recipes');
 const submit = document.getElementById('submit');
 submit.addEventListener('click', fetchData);
 
+
+
 function fetchData(event){
   let searchterm = document.getElementById('input').value;
   let url = `http://www.recipepuppy.com/api/?q=${searchterm}`
 
   fetch(url)
   .then((response) => response.json())
-  .then(function(data) {
-    console.log(data);
-    })
+  .then(data => {
 
-  .catch(function(error) {
+  })
+
+  .catch(error => {
     console.log(error);
+
   });
 }
